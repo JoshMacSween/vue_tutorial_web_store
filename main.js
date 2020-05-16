5f78a8e5a66b0b217bc3351b8856dcc487af8b16
Joshua MacSween <joshmacsween@gmail.com>
2020-05-16 14:05:03 -0600

    computed properties

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ var app = new Vue ({
   el: '#app',
   data: {
     product: 'Socks',
-    image: 'vmSocks-green-onWhite.jpg',
+    brand: "Vue Mastery",
+    selectedVariant: 0,
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-    inventory: 10, 
-    inStock: true,
+    inventory: 10,
     sale: true,
     details: ["80% Cotton", "20% Polyester", "Gender Neutral"],
     variants: [{
         variantId: 2234,
         variantColor: "green",
-        variantImage: 'vmSocks-green-onWhite.jpg'
+        variantImage: 'vmSocks-green-onWhite.jpg',
+        variantQuantity: 10,
       },
 
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "vmSocks-blue-onWhite.jpg"
+        variantImage: "vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
       }],
     sizes: ["Small", "Medium", "Large"],
     cart: 0
@@ -30,8 +32,20 @@ var app = new Vue ({
       this.cart -= 1
     },
 
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+      console.log(index)
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     }
   }
 })
